f500edd61e135827e41e7288bec52e227e7a7d0e
Ionut Ojica <ionut.ojica@yahoo.com>
2024-09-22 19:43:28 +0200

    corrected afisarea textelor pt ssl_certificate si send_pass_to_sender_email

--- script-e40-summary.sh
+++ script-e40-summary.sh
@@ -8,13 +8,23 @@ echo
 echo
 echo '==========================================================================================================================='
 echo
-echo "Mautic installation is finished!${SSL_CERTIFICATE:+ SSL certificate was installed only for test!}"
+echo "Mautic installation is finished!
+if [ "${SSL_CERTIFICATE,,}" == "test" ]; then
+  echo 'SSL certificate was installed only for test!'
+  echo
+elif [ "${SSL_CERTIFICATE,,}" == "yes" ]; then
+  echo 'A production ready SSL certificate was installed.'
+  echo
+else
+  echo 'No SSL certificate was installed.'
+  echo
+fi
 echo
 echo "You can login to Mautic here: https://${MAUTIC_SUBDOMAIN}"
 echo "use username: ${MAUTIC_USERNAME}"
 echo "and password: ${MAUTIC_ADMIN_PASSWORD}"
 echo
-echo "Passwords created during installations were sent to: ${SEND_PASS_TO_SENDER_EMAIL:+$SENDER_EMAIL and to }$ADMIN_EMAIL"
+echo "Passwords created during installations were sent to $(check_positive "${SEND_PASS_TO_SENDER_EMAIL}" && echo "${SENDER_EMAIL} and to ")${ADMIN_EMAIL}"
 echo
 echo '==========================================================================================================================='
 echo 'Updated for Mautic 4 by Ionuţ Ojică - contact@ionutojica.com'
