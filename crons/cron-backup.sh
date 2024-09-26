b3643f6bbc07a62bdc9a9a11bd84cae7b84744f9
Ionut Ojica <ionut.ojica@yahoo.com>
2024-09-26 18:43:56 +0200

    translated to english

--- crons/cron-backup.sh
+++ crons/cron-backup.sh
@@ -4,31 +4,31 @@ source "${current_dir}/mautic.sh"
 NOW=$(TZ="Europe/Berlin" date +"%Y-%m-%d-%H-%M")
 
 cd "${BACKUP_FILES_FOLDER}"
-# Fac copie de rezervă a bazei de date Mautic
+# Backup copy of Mautic database
 mysqldump -u root -p${MYSQL_ROOT_PASSWORD} --single-transaction --quick --lock-tables=false --no-tablespaces "mautic${MAUTIC_COUNT}" > "mautic-database-${NOW}.sql"
 zip "mautic-database-${NOW}.zip" "mautic-database-${NOW}.sql" > /dev/null
 rm "mautic-database-${NOW}.sql"
 
-# Şterg cache-ul
+# Delete cache files
 rm -rf "${MAUTIC_FOLDER}var/cache/*"
 rm -rf "${MAUTIC_FOLDER}var/tmp/*"
-# Fac copie de rezervă a fişierelor Mautic
+# Backup copy of Mautic files
 zip -rq "mautic-files-${NOW}.zip" "${MAUTIC_FOLDER}"
 
-# Refac permisiunile dosarului Mautic
+# Assign permissions for Mautic folder
 #chown -R www-data:www-data "${MAUTIC_FOLDER}"
 #chmod -R 755 "${MAUTIC_FOLDER}"
 #. $dosar_fisiere_root'reset-mautic-permissions.sh'
 
-# Reîmprospătez cache-ul
-# Suprim output-ul (iesirea) comenzii, redirectionand iesirea standard (stdout) catre null: > /dev/null
-# Redirectionez si iesirea de eroare (stderr) catre null: 2>&1
+# Recreate cache
+# Silence output, redirecting standard output (stdout) to null: > /dev/null
+# Redirecting also error output (stderr) to null: 2>&1
 php "${MAUTIC_FOLDER}bin/console cache:warmup" --no-interaction > /dev/null 2>&1
 
-# Şterg copiile de rezervă mai vechi de 8 zile
+# Delete backup copies older than 8 days
 find "${BACKUP_FILES_FOLDER}*" -mtime +8 -exec rm {} \;
 
-# Copiile de rezervă "${BACKUP_FILES_FOLDER}"mautic-database-${NOW}.zip şi mautic-files-${NOW}.zip sunt create
+# Following backup files are created: "${BACKUP_FILES_FOLDER}"mautic-database-${NOW}.zip and mautic-files-${NOW}.zip
 
-# Şterg jurnalele mai vechi de 30 zile
+# Delete also logs older than 30 days
 find "${CRON_FOLDER}log/*" -mtime +30 -exec rm {} \;
