88ba476a5f058bbb8539e35ab0d633bd4f6c71ef
Ionut Ojica <ionut.ojica@yahoo.com>
2024-09-26 18:44:22 +0200

    folder corrected

--- crons/switchCronJobs.sh
+++ crons/switchCronJobs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 . $(dirname "$(readlink -f "$0")")'/mautic.sh'
-if [ -e "${MAUTIC_FOLDER}DO-NOT-RUN" ]; then
-  mv "${MAUTIC_FOLDER}DO-NOT-RUN" "${MAUTIC_FOLDER}DO--RUN"
+if [ -e "${CRON_FOLDER}DO-NOT-RUN" ]; then
+  mv "${CRON_FOLDER}DO-NOT-RUN" "${CRON_FOLDER}DO--RUN"
 elif [ -e "/var/mautic-crons/DO--RUN" ]; then
-  mv "${MAUTIC_FOLDER}DO--RUN" "${MAUTIC_FOLDER}DO-NOT-RUN"
+  mv "${CRON_FOLDER}DO--RUN" "${CRON_FOLDER}DO-NOT-RUN"
 else
-  touch "${MAUTIC_FOLDER}DO-NOT-RUN"
+  touch "${CRON_FOLDER}DO-NOT-RUN"
 fi
